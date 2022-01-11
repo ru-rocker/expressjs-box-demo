1dd019a9b5fd7cf5ce69b28e39ba574f7383dd48
ru-rocker <ru.rocker81@gmail.com>
2022-01-11 19:22:09 +0700

    add host information

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ ExpressJS demo. For showing box only with color.
 Built on top of docker, with tags represent its color.
 
 The docker image is pushed in https://hub.docker.com/r/rurocker/boxdemo.
+
+
+# Run
+
+    #red
+    docker run --rm -p3000:3000 rurocker/boxdemo:red
+
+    #green
+    docker run --rm -p3000:3000 rurocker/boxdemo:green
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var os = require('os');
 var router = express.Router();
+const hostName = os.hostname();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let color = 'red';
-  res.render('index', { title: 'Box Demo', boxcolor: color });
+  let color = 'green';
+  res.render('index', { title: 'Box Demo', boxcolor: color, hostName: hostName });
 });
 
 module.exports = router;
