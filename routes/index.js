4cb64c97b292f0b1128d02e25f9c561b37469261
ru-rocker <ru.rocker81@gmail.com>
2022-01-11 19:15:17 +0700

    add hostnames

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var os = require('os');
+
 var router = express.Router();
+const hostName = os.hostname();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let color = process.env.BOX_COLOR || 'black';
-  res.render('index', { title: 'Box Demo', boxcolor: color });
+  res.render('index', { title: 'Box Demo', boxcolor: color, hostName: hostName });
 });
 
 module.exports = router;
