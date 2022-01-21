13b4750a6003030bc95312d4ca231e5625621b2f
ru-rocker <ru.rocker81@gmail.com>
2022-01-21 08:39:57 +0700

    add filesystem

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var os = require('os');
+const fs = require('fs');
 
 var router = express.Router();
 const hostName = os.hostname();
@@ -7,19 +8,21 @@ const hostName = os.hostname();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let color = process.env.BOX_COLOR || 'black';
-  var cookie = req.cookies.cookieName;
-  if (cookie === undefined) {
-    // no: set a new cookie
-    var randomNumber=Math.random().toString();
-    randomNumber=randomNumber.substring(2,randomNumber.length);
-    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
-  } else {
-    // yes, cookie was already present 
-    console.log('cookie exists', cookie);
-  } 
-  
-  res.render('index', { title: 'Box Demo', boxcolor: color, hostName: hostName });
+  let dir = process.env.DIR || '/tmp';
+  let fn = process.env.FILENAME || 'testfile.txt';
+
+  var fileContents;
+  try {
+    fileContents = fs.readFileSync(dir + '/' + fn, 'utf8');
+  } catch (err) {
+  }
+
+  res.render('index', { 
+    title: 'Box Demo', 
+    boxcolor: color, 
+    fileContents: fileContents,
+    dir: dir,
+    hostName: hostName });
 });
 
 module.exports = router;
