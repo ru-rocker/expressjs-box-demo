f5c6cb5b5e194a95c7c9fc5630e9afb4e67a7d68
ru-rocker <ru.rocker81@gmail.com>
2022-01-21 08:16:53 +0700

    add cookie

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,18 @@ const hostName = os.hostname();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let color = process.env.BOX_COLOR || 'black';
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    var randomNumber=Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  
   res.render('index', { title: 'Box Demo', boxcolor: color, hostName: hostName });
 });
 
